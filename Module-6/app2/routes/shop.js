3a291470cc6ab79ddac1a8ce1bc9d22b84975c41
jitendra <gangwarjitendra2498@gmail.com>
2022-03-07 18:01:28 +0530

    Update Module-6

---
+++ Module-6/app2/routes/shop.js
@@ -0,0 +1,16 @@
+const path=require("path")
+const express=require("express");
+const rootDir=require('../util/path');
+const adminData=require('./admin');
+const router=express.Router();
+
+//get directly target the path like '/' otherwise get the error
+router.get('/',(req,res,next)=>{
+    //res.sendFile(path.join(__dirname,'../',"views","shop.html"));//send the resonse to the server
+    const products=adminData.products;
+    res.render('shop',{prods:products,docTitle:'Shop'});
+});
+
+module.exports=router;
+
+
