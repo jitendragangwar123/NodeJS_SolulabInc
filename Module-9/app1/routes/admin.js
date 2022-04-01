e2493056ea1ac91ed2286b57b8e14da71dec5633
jitendra <gangwarjitendra2498@gmail.com>
2022-04-01 23:53:56 +0530

    update app1

--- Module-9/app1/routes/admin.js
+++ Module-9/app1/routes/admin.js
@@ -15,4 +15,5 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-module.exports = router;
+module.exports = router; 
+
